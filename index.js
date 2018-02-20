1368b3beef72487bdd524c2dbd74d3c64f218665
Ozi <ozi@Ozis-MacBook-Pro.local>
2018-02-20 14:27:09 +0700

    release 0

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file,cb) {
   // your code here...
+  fs.readFile(parent_file, 'utf8', (err, parent_data)=>{  
+    if (err) throw err;
+    parent_data = JSON.parse(parent_data);
+    parent_data.map((v,i,a)=>{
+      v.children =[];
+    });
+    fs.readFile(children_file, 'utf8', (err, children_data)=>{  
+      if (err) throw err;
+      children_data = JSON.parse(children_data);
+      parent_data.forEach((v,i,a)=>{
+        children_data.forEach((v2,i2,a2)=>{
+          if(v.last_name === v2.family) v.children.push(v2.full_name);
+        });
+        sleep.sleep(3);
+        cb(parent_data);
+      });
+  });
+});
+
+
 }
 
-match_data('./parents.json', './childrens.json')
+match_data('./parents.json', './childrens.json',(data)=>{
+  console.log(data);
+});
 console.log("Notification : Data sedang diproses !");
