7ec8bb725163b614819ef3a5a437865830fbb941
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-02-20 15:06:42 +0700

    Feedback

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+function readFile(data,callback){
+  fs.readFile(data,'UTF-8',(err,getData)=>{
+    let parse = JSON.parse(getData)
+    callback(parse)
+  })
 }
 
-match_data('./parents.json', './childrens.json')
+function match_data(parent_file, children_file,cb) {
+  readFile(parent_file,(getData)=>{
+    readFile(children_file,(getDataChildren)=>{
+      let parent = getData, children = getDataChildren
+      
+      const newData = parent.map(each =>{
+        const checkFam = children.reduce((total,eachChildren)=>{
+          if(each.last_name === eachChildren.family){
+            total.push(eachChildren.full_name)
+          }
+          return total
+        },[])
+
+        each.childrens = checkFam
+        return each
+      })
+
+      cb(newData)
+    })
+  })
+}
+
+
+
+match_data('./parents.json', './childrens.json',function show(data){
+  console.log(data)
+})
 console.log("Notification : Data sedang diproses !");
+sleep.sleep(5)
+
