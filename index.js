6330ff3b404ed6d382735fbfc6ea0c63c1feac84
Fitrul Islam <fitrul.islam@gmail.com>
2018-02-20 15:23:31 +0700

    done

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const fs = require('fs');
+const views = require("./view.js");
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+function match_data(parent_file, children_file, callback) {
+  fs.readFile(parent_file, 'UTF-8', function(err,data){
+    sleep.sleep(5);
+    let parentsData = JSON.parse(data);
+    fs.readFile(children_file, 'UTF-8', function(err,data){
+      sleep.sleep(5);
+      let childrensData = JSON.parse(data);
+      for(let i=0; i<parentsData.length; i++){
+        parentsData[i].childrens = [];
+        for(let j=0; j<childrensData.length; j++){
+          if(parentsData[i].last_name===childrensData[j].family){
+            parentsData[i].childrens.push(childrensData[j].full_name);
+          }
+        }
+      }
+      callback(parentsData);
+    });
+  });
 }
 
-match_data('./parents.json', './childrens.json')
+match_data('./parents.json', './childrens.json', views);
 console.log("Notification : Data sedang diproses !");
