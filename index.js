404dcd6dd46a7192a113ed28ee218b256734d9cf
Andrew Kusuma <andrew.budikusuma@gmail.com>
2018-02-20 13:21:02 +0700

    Release 0

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ var sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // your code here...
+  let parent_data = []
+  fs.readFile('./parents.json', 'utf-8', (errparent, dataparent) => {
+    fs.readFile('./childrens.json', 'utf-8', (errchildren, datachildren) => {
+      sleep.sleep(5)
+      let children = JSON.parse(datachildren)
+      let parent = JSON.parse(dataparent)
+      for (let index = 0; index < parent.length; index++) {
+        parent_data.push(parent[index])
+        parent_data[index].children = []
+        for (let indexChildren = 0; indexChildren < children.length; indexChildren++) {
+          if (children[indexChildren].family === parent_data[index].last_name) {
+            parent_data[index].children.push(children[indexChildren].full_name)
+          }
+        }
+      }
+      console.log(parent_data)
+    })
+  })
 }
 
 match_data('./parents.json', './childrens.json')
