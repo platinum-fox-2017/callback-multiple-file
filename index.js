0dcc29b91f550aed2a93ff7c381e264261c656e7
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-02-20 14:06:04 +0700

    finished

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ var sleep = require('sleep');
 
 function match_data(parent_file, children_file) {
   // your code here...
+  fs.readFile(parent_file,'utf8',function(err,data){
+      sleep.sleep(5);
+      let parent_data = JSON.parse(data);
+      fs.readFile(children_file,'utf8',function(err2,data2){
+          sleep.sleep(5);
+          let childrens = JSON.parse(data2);
+          for(let i = 0; i < parent_data.length; i++){
+              parent_data[i].childrens = new Array();
+              for(let j = 0; j < childrens.length; j++){
+                  if(parent_data[i].last_name == childrens[j].family){
+                      parent_data[i].childrens.push(childrens[j]);
+                  }
+              }
+          }
+          console.log(parent_data);
+      })
+  });
 }
 
 match_data('./parents.json', './childrens.json')
