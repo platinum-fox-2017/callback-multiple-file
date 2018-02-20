6fbd4ada7ee6339986615c9b3f234925854c940c
adhiartaputra <adhiartaprabataputra@gmail.com>
2018-02-20 13:40:50 +0700

    release 0

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, callback) {
   // your code here...
+  fs.readFile(parent_file,'utf-8',(err,parent) => {
+    let parents = JSON.parse(parent)
+    fs.readFile(children_file,'utf-8',(err,child) => {
+      let children = JSON.parse(child)
+      for(let i = 0; i < parents.length; i++) {
+        parents[i].children = []
+        for(let j = 0; j < children.length; j++) {
+          if(children[j].family === parents[i].last_name) {
+            parents[i].children.push(children[j].full_name)
+          }
+        }
+      }
+      sleep.sleep(5)
+      callback(parents)
+    })
+  })
 }
 
-match_data('./parents.json', './childrens.json')
+match_data('./parents.json', './childrens.json',parents => {
+  console.log(parents);
+})
 console.log("Notification : Data sedang diproses !");
